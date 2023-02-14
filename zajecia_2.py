cece4b7adc58b7c3dd6f8609beb2d84c3d43bce2
Patryk <patryk.ciwinski@gmail.com>
2023-02-14 19:24:10 +0100

    some training

--- zajecia_2.py
+++ zajecia_2.py
@@ -9,7 +9,7 @@ else:
 
 print(f"W temperaturze {temp} st. C woda to {water}")
 
-salary= 2212
+salary= 7100
 
 if salary >=4000 and salary<=7000:
     podatek = salary * 0.15
@@ -27,3 +27,9 @@ elif day >= 8 or day <= 1:
     print("nieprawidłowe dane")
 else:
     print(f"do weekendu pozostało {(c:=5-day)} dni")
+
+
+a = "Zarabiasz powyżej przeciętną" if salary>7000 else "zarabiasz przeciętną"if 4000<=salary<=7000 else \
+    "zarabiasz poniżej przeciętnej"
+
+print(a)
